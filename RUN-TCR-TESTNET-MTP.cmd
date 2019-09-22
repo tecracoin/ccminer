e5a91aeb38a6f92771495b64a7b2d149a77f8484
partyka1 <maciek@partyka.io>
2019-09-22 18:27:59 +0200

    Update and rename RUN-TECRA-MTP.cmd to RUN-TCR-TESTNET-MTP.cmd

---
+++ RUN-TCR-TESTNET-MTP.cmd
@@ -0,0 +1,4 @@
+
+rem x64\Release\ccminer -a mtp -o  http://127.0.0.1:8382   -u djm34 -p password --coinbase-addr GJVkKsPFdBsavo5wn5WGiCSScgFZE2F27C  --no-getwork -i 20
+x64\Release\ccminer -a mtp -o stratum+tcp://pool-djm.tecracoin.io:4554 -u GJVkKsPFdBsavo5wn5WGiCSScgFZE2F27C  -p 0 
+pause
