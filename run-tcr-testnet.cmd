f8d17776ee365914c1c07c3245b0a42891af581e
partyka1 <maciek@partyka.io>
2019-09-22 19:20:28 +0200

    Update and rename RUN-TCR-TESTNET-MTP.cmd to run-tcr-testnet.cmd

--- run-tcr-testnet.cmd
+++ run-tcr-testnet.cmd
@@ -1,4 +1,5 @@
-
+# solo mining:
 rem x64\Release\ccminer -a mtp -o  http://127.0.0.1:8382   -u tcrminer -p password --coinbase-addr GJVkKsPFdBsavo5wn5WGiCSScgFZE2F27C  --no-getwork -i 20
-x64\Release\ccminer -a mtp -o stratum+tcp://pool-djm.tecracoin.io:4554 -u GJVkKsPFdBsavo5wn5WGiCSScgFZE2F27C  -p 0 
+# pool mining:
+x64\Release\ccminer -a mtp -o stratum+tcp://pool.testnet.tecracoin.io:4554 -u GJVkKsPFdBsavo5wn5WGiCSScgFZE2F27C  -p 0 
 pause
