d512d6de73666134bfa5bb479c61269a20ced4f0
partyka1 <maciek@partyka.io>
2019-09-22 18:29:18 +0200

    Create RUN-TCR-MTP.cmd

---
+++ RUN-TCR-MTP.cmd
@@ -0,0 +1,3 @@
+rem x64\Release\ccminer -a mtp -o  http://127.0.0.1:8382   -u tcrminer -p password --coinbase-addr TC4frBMpSm2PF2FuUNqJ3qicn4EHL59ejL  --no-getwork -i 20
+x64\Release\ccminer -a mtp -o stratum+tcp://pool.tecracoin.io:4553 -u TC4frBMpSm2PF2FuUNqJ3qicn4EHL59ejL  -p 0 
+pause
