e3e0866231325b81098ac5d741959d768d403984
djm34 <djm34@aliceadsl.fr>
2019-09-26 22:50:37 +0300

    fix issue causing crash on start on linux

---
+++ RUN-TECRA-MTP.cmd
@@ -0,0 +1,5 @@
+
+rem x64\Release\ccminer -a mtp -o  http://127.0.0.1:8382   -u djm34 -p password --coinbase-addr aDn7MMYjVQqenT11VFDYHfFdwXmSTRUTak  --no-getwork -i 20 --protocol-dum --debug
+x64\Release\ccminer -a mtp-tcr -o stratum+tcp://pool-djm.tecracoin.io:4554 -u GP2ZR6Hpjc7ffdV8jRXVJvzfS9EPbRdkbT -p 0 -d 0 
+rem x64\Release\ccminer\ccminer -a mtp -o stratum+tcp://pool-djm.tecracoin.io:4554 -u Gfa27Gve3keHbb7BMAfQmvHdMJmhrAykaF -p 0 -d 0
+pause
--- run-tcr-testnet.cmd
+++ run-tcr-testnet.cmd
@@ -1,5 +1,5 @@
 rem solo mining:
 rem x64\Release\ccminer -a mtp-tcr -o  http://127.0.0.1:8382   -u tcrminer -p password --coinbase-addr GJVkKsPFdBsavo5wn5WGiCSScgFZE2F27C  --no-getwork -i 20
 rem pool mining:
-x64\Release\ccminer -a mtp-tcr -o stratum+tcp://pool.testnet.tecracoin.io:4554 -u GJVkKsPFdBsavo5wn5WGiCSScgFZE2F27C  -p 0 
+x64\Release\ccminer -a mtp-tcr -o stratum+tcp://pool.testnet.tecracoin.io:4554 -u GUvmFuNNNL3GdebK4EZjTxRZraEgQ2YjaF -p 0 
 pause
